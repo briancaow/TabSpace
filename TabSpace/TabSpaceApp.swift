7c77ac49393485f08b697c5920755bf46c1f9b35
briancaow <briancao02@gmail.com>
2023-02-25 16:14:33 -0800

    Initial Commit

---
+++ TabSpace/TabSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabSpaceApp.swift
+//  TabSpace
+//
+//  Created by Brian Cao on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct TabSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
